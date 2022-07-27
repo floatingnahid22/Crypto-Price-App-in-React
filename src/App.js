7f4ac7f544053f5652039dd187eb48b5d1a9b10d
floatingnahid22 <nahid.uap.github@gmail.com>
2022-07-28 01:59:33 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Coin from "./components/Coin";
 
 function App() {
-  return App;
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
+
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
+      (response) => {
+        setListOfCoins(response.data.coins);
+      }
+    );
+  }, []);
+
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
+
+  return (
+    <div className="App">
+      <div className="cryptoHeader">
+        <input
+          type="text"
+          placeholder="Bitcoin..."
+          onChange={(event) => {
+            setSearchWord(event.target.value);
+          }}
+        />
+      </div>
+      <div className="cryptoDisplay">
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              name={coin.name}
+              icon={coin.icon}
+              price={coin.price}
+              symbol={coin.symbol}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
